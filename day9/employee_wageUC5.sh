f837625cfdaa22aee63c3cd61eb4cc8a40835be6
abhishekboralkar <boralkarabhishek@gmail.com>
2022-05-02 17:19:43 +0530

    One month sallary clcu.UC5

---
+++ day9/employee_wageUC5.sh
@@ -0,0 +1,23 @@
+#one month sallary calcultion
+Present=$((RANDOM%3));
+perhourSalary=20;
+workinghour=0;
+if [ $Present -eq 0 ]
+then
+        echo "Employee is absent";
+        workingHour=0;
+elif [ $Present -eq 1 ]
+then
+        echo "Employee is present";
+        workinghour=8;
+else
+        echo "Employee is working as part time";
+        workinghour=4;
+fi
+salary=$(($perhourSalary * $workinghour));
+
+echo "Employee has earned $salary $ today";
+month=$(($salary * 20 ));
+echo "Total One month Sallary (20days) is : $month $ "
+
+
