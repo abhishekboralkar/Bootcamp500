5e4ee790da8299b99f5e7876348c8eecb893973b
abhishekboralkar <boralkarabhishek@gmail.com>
2022-05-02 17:02:39 +0530

    employeewage by using CASEuc4

---
+++ day9/employeewage_uc4.sh
@@ -0,0 +1,25 @@
+Present=$((RANDOM%3));
+perhourSalary=20;
+workinghour=0;
+case $Present in
+        0)
+        echo "Employee is absent";
+        workinghour=0;
+        ;;
+
+        1)
+        echo "Employee is present";
+        workinghour=8;
+        ;;
+
+        2)
+        echo "Employee is working as part time";
+        workinghour=4;
+        ;;
+esac
+salary=$(($perhourSalary * $workinghour));
+echo "Employee has earned $salary $ today";
+
+
+
+
